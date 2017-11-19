5a7e73510d16bfe3ddf63deb3045a7d69be6102c
Nithin Betegeri <nithinpb@gmail.com>
2017-11-19 20:41:38 +0530

    Add gulp task for js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-clean-css');
+var browserify = require('browserify');
+var es6ify = require('es6ify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+// var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
   scripts: './project/web/frontend/static/js/app.js',
@@ -10,6 +16,21 @@ var paths = {
   vendor_styles: 'project/web/frontend/static/css/*.css',
   styles: 'project/web/frontend/static/less/*.less'
 };
+
+gulp.task('scripts', function () {
+    es6ify.traceurOverrides = {experimental: true};
+
+    return browserify(paths.scripts)
+        .transform(reactify)
+        .transform(es6ify)
+        .bundle()
+        .pipe(source('app.js'))
+        // .pipe(streamify(sourcemaps.init()))
+        .pipe(streamify(concat('app.min.js')))
+        // .pipe(uglify())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('project/web/frontend/static/dist/js'));
+});
 
 gulp.task('vendor-styles', function () {
     return gulp.src([paths.vendor_styles])
@@ -31,4 +52,4 @@ gulp.task('watch', function() {
   gulp.watch(paths.styles, ['styles']);
 });
 
-gulp.task('default', ['watch', 'vendor-styles', 'styles']);
+gulp.task('default', ['watch', 'scripts', 'vendor-styles', 'styles']);
