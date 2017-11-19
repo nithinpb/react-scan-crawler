1ef0baadbdfd54f9958b66991eade2326da36614
Nithin Betegeri <nithinpb@gmail.com>
2017-11-19 20:35:36 +0530

    Added gulp tasks for css minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
+var minifyCSS = require('gulp-clean-css');
 
 var paths = {
   scripts: './project/web/frontend/static/js/app.js',
@@ -10,9 +11,24 @@ var paths = {
   styles: 'project/web/frontend/static/less/*.less'
 };
 
+gulp.task('vendor-styles', function () {
+    return gulp.src([paths.vendor_styles])
+        .pipe(concat('vendor.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('project/web/frontend/static/dist/css'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src([paths.styles])
+        .pipe(less())
+        .pipe(concat('app.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('project/web/frontend/static/dist/css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.app_paths, ['scripts']);
   gulp.watch(paths.styles, ['styles']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'vendor-styles', 'styles']);
